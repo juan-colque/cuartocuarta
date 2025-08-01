040854f11336c7400cbcc28b05e66e77f7abac6d
Juan Javier <juancolque2483@gmail.com>
2025-08-01 17:14:01 +0200

    entrega de tarea

---
+++ 01_funciones/tarea/index.js
@@ -0,0 +1,18 @@
+
+
+alert("Bienvenia nuetra calculadora");
+
+// Creamos las variables para la SUMA
+let numeroUno = prompt("Introduce el primer número");
+let numeroDos = prompt("Introduce el segundo número");
+
+// Convertimos en numeros los datos
+numeroUno = parseInt(numeroUno);
+numeroDos = parseInt(numeroDos);
+
+// Provamos si capturamos los datos 
+console.log("Los números que ingresan son" + numeroUno + " y " + numeroDos);
+
+// Creamos la calculadora
+console.log("El resultado de la suma es: " + (numeroUno + numeroDos));
+
